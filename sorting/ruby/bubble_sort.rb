d6b71f5a0744fae1487f4c0babbd97a7a28112e9
ephreal <tjannx@hotmail.com>
2019-12-26 14:42:26 -0700

    Started adding in ruby files

---
+++ sorting/ruby/bubble_sort.rb
@@ -0,0 +1,26 @@
+# Ruby implementation of bubble sort.
+# See documentation.needs.management/index.php/Sorting_Algorithms for
+# the algorithm followed.
+
+def bubble_sort(x)
+    x = x[0,x.length()]
+    x_range = *(0 ... x.length()-1)
+    swapped = true
+    while swapped
+        swapped = false
+        for y in x_range
+            if x[y] > x[y+1]
+                temp = x[y]
+                x[y] = x[y+1]
+                x[y+1] = temp
+                swapped = true
+            end
+        end
+    end
+    return x
+end
+
+x = [2, 5, 1, 0, 4, 66, 123, 864, -0, 11, -3234]
+puts x.join(" ")
+y = bubble_sort(x)
+puts y.join(" ")
