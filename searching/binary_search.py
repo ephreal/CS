9d1ee8e53bbb96094ad346bca3116183e6723343
ephreal <tjannx@hotmail.com>
2019-12-27 15:57:41 -0700

    Added in start of binary search

---
+++ searching/binary_search.py
@@ -0,0 +1,8 @@
+# See documentation.needs.management/index.php/Search_Algorithms for
+# the algorithm followed.
+
+
+def binary_search(x, item):
+    """
+    Binary search implemented in python
+    """
