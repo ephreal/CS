1ee2baf1eed161fec908990295870c06a2689e33
ephreal <tjannx@hotmail.com>
2019-12-30 09:13:40 -0700

    Completed binary search

--- searching/binary_search.py
+++ searching/binary_search.py
@@ -6,3 +6,37 @@ def binary_search(x, item):
     """
     Binary search implemented in python
     """
+
+    # Start at the center of the search space (or as close as possible)
+    highest = len(x)
+    lowest = 0
+    index = len(x)//2
+    while not x[index] == item:
+        if index < 0 or highest == lowest:
+            # item not in the list
+            return
+        if x[index] < item:
+            lowest = index
+            mod = (highest - lowest)//2
+            if mod == 0:
+                # Item is above the range of the list
+                return
+            index += mod
+        elif x[index] > item:
+            highest = index
+            index -= (highest) - (lowest)
+    return index
+
+
+def run_search(x, item):
+    y = binary_search(x, item)
+    if y is not None:
+        print(f"Found at index {y}, x[{y}]: {x[y]}")
+    else:
+        print("Error, item not found")
+
+
+x = [1, 2, 3, 4, 5]
+
+for i in range(0, 7):
+    run_search(x, i)
