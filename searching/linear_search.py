2b21e88be72ce5ff16b07b318c7b63aa5b123855
ephreal <tjannx@hotmail.com>
2020-01-13 06:30:34 -0700

    Added in linear search

---
+++ searching/linear_search.py
@@ -0,0 +1,15 @@
+def linear_search(searchable_list, target):
+    """
+    Basic linear search implemented in python
+    """
+
+    for i in range(0, len(searchable_list)-1):
+        if searchable_list[i] == target:
+            return i
+
+    return None
+
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+y = linear_search(x, 10)
+print(y)
