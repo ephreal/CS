bd561fcdac0dfa3178e157dea8984439864d8434
ephreal <tjannx@hotmail.com>
2020-01-13 06:31:36 -0700

    fixed off-by-one issue

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -3,7 +3,7 @@ def linear_search(searchable_list, target):
     Basic linear search implemented in python
     """
 
-    for i in range(0, len(searchable_list)-1):
+    for i in range(0, len(searchable_list)):
         if searchable_list[i] == target:
             return i
 
@@ -11,5 +11,5 @@ def linear_search(searchable_list, target):
 
 x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
 
-y = linear_search(x, 10)
+y = linear_search(x, 9)
 print(y)
